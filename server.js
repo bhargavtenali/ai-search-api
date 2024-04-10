80e3431385f985702546c6b7e74a0d12b854d76f
Bhargav Tenali <bhargavtenali@gmail.com>
2024-04-10 15:03:45 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 8080;
+
+app.use(cors());
+
+app.get("/api/home", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
