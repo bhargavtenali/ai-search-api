ef246e37b238763cddb0e5c4dacf8713e1f8f0d6
Bhargav Tenali <bhargavtenali@gmail.com>
2024-04-18 20:24:33 +0530

    chore: ai prompt improved

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -12,32 +12,39 @@ const generateMeta = async (req, res) => {
     messages: [
       {
         role: "system",
-        content: "You are a popular search engine and mongoDB expert.",
+        content: `You are a conversational search expert who understands user queries typed in Natural Language and converts them to MongoDb queries, 
+          so that the required data can be extracted and presented to the user.
+          In the User Request to the API, you will be given the Fields Array which is array of objects, where each object has multiple keys and corresponding values.
+          Out of them uiFieldName, filterFieldName and rootFieldName keys are your fields of interest.
+          You will also be given the query made by the user in Natural Language.
+          We have a mongoDB database where the value of rootFieldName is one of fields
+          You will also be given 1 record of our mongoDB database as sample data.
+          In our backend, we use mongoose doing operations on our mongoDB Database.
+          
+
+          You have to perform three tasks 
+
+          Task 1 - Understand the user query and try to match any keyword or phrase or substring to the uiFieldName and / or the filterFieldName from the Fields Array provided to you.
+          Guidelines on how to send the response: Give me uiFieldName, filterFieldName, rootFieldName for every match as response. And for every match give the corresponding keyword or phrase or substring of user query against key matchedSubString in the response.
+          give response as an array against key matchedFields in json object
+          
+          Task 2 - you need to construct Mongoose find query with matched rootFieldName fields and their values (extracted from user query).
+          construct query in such a way that it gives relevant information for user query.
+          Consider present date and time as reference while constructing find query for user queries where time duration is mentioned.
+          Guidelines on how to send the response: give me the filter, projection, options for Mongoose find() query as json object against key mongoDBQuery.
+
+          Task 3 - user query might not be fully complete.
+          give me upto 2 autocomplete predictions for user query and corresponding matchedFields and corresponding mongoDBQuery.
+          Guidelines on how to send the response: give them as json object against key autocomplete.
+          add 3 tasks add guidelines explain what it needs to be sent as response
+          keep varialbe data only in user rest in system
+          `,
       },
       {
         role: "user",
-        content: `You are given array of objects (fieldsArray) - ${JSON.stringify(
-          fieldsArray
-        )}
-        Each object has multiple keys and corresponding values. Out of them uiFieldName, filterFieldName and rootFieldName keys are my fields of interest.
-        You are also given user entered search text (userInput) - ${userInput}
-        give me the objects in fieldsArray whose uiFieldName or filterFieldName field values are present in userInput or best matching with any words of userInput.
-        give uiFieldName, filterFieldName, rootFieldName of matched objects as response. give response as an array against key matchedFields in json object
-        for every matchedField also give the corresponding part of userInput that matched with that field against key matchedSubString
-
-        We have a mongoDB database where rootFieldName is actual db field for the corresponding object.
-        This is sample data of our mongoDB database - ${JSON.stringify(
-          mongoRecords1
-        )}
-        I want to construct a chart for the userInput search text. But I need to query required data from our mongoDB database
-        you need to construct mongoDB query with matched rootFieldName fields and their values (extracted from userInput).
-        construct query in such a way that it gives relevant information for constructing a chart based on userInput search text.
-        I have to run db query through Mongoose in the node express server
-        give me the parameters for mongoose find() function along with above response as json object with key mongoDBQuery.
-        
-        userInput might not be fully complete.
-        give me upto 2 autocomplete predictions for userInput and corresponding matchedFields and corresponding mongoDBQuery.
-        give them along with above response as json object with key autocomplete.
+        content: `Fields Array - ${JSON.stringify(fieldsArray)}
+        user query - ${JSON.stringify(userInput)}
+        sample data of our mongoDB database - ${JSON.stringify(mongoRecords1)}
         `,
       },
     ],
